b634c4041f083e84f0bbe1a66424f461870f36e0
Mark Goldthorp <code@koalatea-software.com>
2020-09-10 18:47:13 +0100

    skeletons for the projects page

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-### Hi there 👋
-
-<!--
-**KoalaTeaSoftware/koalateasoftware** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-
-Here are some ideas to get you started:
-
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+# Koala Tea Software Site
+## Friendly URLs
+* Index.php will check to see that there are a suitable
+  * Chapter
+  * Section
+  * Subsection
+* It determines 'suitability' in the following way(s)
+  * Chapter
+    * Must have a directory with a matching name (lower case)
+    * Must contain a file called contents.php
+  * Section
+    * There has to be a directory (with a name matching the request) within the requested chapter
+    * Not (currently) demanding that there be a contents.php in the section directory
+    
+## Auto Creation of the main menu
+* This depends on the directory structure, too
+* A main-nav item will be created for each of the directories (under the root) that
+    * contain a contents.php
+    * are not called 'home', or named with an initial '_' 
--- koalateasoftware.com/projects/contents.php
+++ koalateasoftware.com/projects/contents.php
@@ -1,2 +1,55 @@
 <h1>Projects</h1>
+<ul class="nav nav-tabs" id="rolesNav" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link active" id="p1-tab" data-toggle="tab" href="#p1" role="tab"
+           aria-controls="home"
+           aria-selected="true">Rose Goldthorp</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" id="p2-tab" data-toggle="tab" href="#p2" role="tab"
+           aria-controls="profile"
+           aria-selected="false">Project 2</a>
+    </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+    <div class="tab-pane fade show active" id="p1" role="tabpanel" aria-labelledby="p1-tab">
+        <div class="row">
+            <div class="col">
+                <h2>Agile(ish) Test Plan</h2>
+                <?php include "rose-goldthorp/test-plan.htm" ?>
+            </div>
+            <div class="col">
+                <h2>Test Results</h2>
+            </div>
+        </div>
+    </div>
+    <div class="tab-pane fade" id="p2" role="tabpanel" aria-labelledby="p2-tab">
+        <div class="row">
+            <div class="col">
+                <h2>Agile(ish) Test Plan for second project</h2>
+                <?php include "rose-goldthorp/test-plan.htm" ?>
+            </div>
+            <div class="col">
+                <h2>Test Results</h2>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(() => {
+        const url = window.location.href;
+        if (url.indexOf("#") > 0) {
+            const activeTab = url.substring(url.indexOf("#") + 1);
+            $('.nav[role="tablist"] a[href="#' + activeTab + '"]').tab('show');
+        }
+
+        $('a[role="tab"]').on("click", function () {
+            const hash = $(this).attr("href");
+            let newUrl = url.split("#")[0] + hash;
+            history.replaceState(null, null, newUrl);
+        });
+    });
+</script>
+
 <?php
+// actually, validate the section here, and take the validation of section and sub-section out of index.php
