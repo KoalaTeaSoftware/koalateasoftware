b26140c78f3fc7e986880c21e4ee504b2328e9af
Mark Goldthorp <code@koalatea-software.com>
2020-09-13 09:12:56 +0100

    * Section validation delegated to the chapter-level processors * added a markdown-processor

--- koalateasoftware.com/projects/contents.php
+++ koalateasoftware.com/projects/contents.php
@@ -1,8 +1,22 @@
 <?php
-$projectsToIgnore[] = "contents.php";
+$projectsToIgnore[] = "";
 /** @noinspection PhpUndefinedVariableInspection */
 require_once $absoluteSiteRoot . "_framework/listSubsections.php";
-$availableProjects = listSubordinates( $absoluteSiteRoot . "projects/*/testPlan.htm", $projectsToIgnore);
+$availableProjects = listSubordinates($absoluteSiteRoot . "projects/*/testPlan.htm", $projectsToIgnore);
+
+/** @noinspection PhpUndefinedVariableInspection */
+if (!in_array($section, $availableProjects)) {
+    error_log("Requested section :" . $section . ": is not one of the known projects." . print_r($availableProjects, true));
+
+    $section = $availableProjects[0];
+    $titleTag = ucwords(str_replace('-', ' ', $section)); // until a chapter or section changes it
+}
+error_log("Section is :" . $section . ":");
+
+
+
+require_once $absoluteSiteRoot . "_framework/PHP Markdown 1.0.2/markdown.php";
+$junk = Markdown("# Hwllo");
 ?>
 <div class="container-fluid">
     <nav id="projNav" class="nav navbar navbar-expand-lg navbar-light bg-light">
@@ -14,9 +28,12 @@ $availableProjects = listSubordinates( $absoluteSiteRoot . "projects/*/testPlan.
         <div class="collapse navbar-collapse" id="projNavAltMarkup">
             <div class="navbar-nav">
                 <?php
-                echo drawNavItems( $availableProjects, "rose-goldthorp","projects/");
+                echo drawNavItems($availableProjects, "rose-goldthorp", "projects/");
                 ?>
             </div>
         </div>
     </nav>
+    <section id="projectInfo">
+        <?= $junk ?>
+    </section>
 </div>
